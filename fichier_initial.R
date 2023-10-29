d6aab879c75c7d2498bad31d764d75dd646fcdb3
MaxenceCHAN07 <144699975+MaxenceCHAN07@users.noreply.github.com>
2023-10-29 10:20:05 +0100

    Update fichier_initial.R

--- fichier_initial.R
+++ fichier_initial.R
@@ -1,3 +1,4 @@
+#Installation des packages si besoin
 if (!require(httr)) {
   install.packages("httr")
 }
@@ -18,18 +19,19 @@ if (!require(RMySQL)) {
   install.packages("RMySQL")
 }
 
+#Chargement des packages
 library(httr)
 library(jsonlite)
 library(dplyr)
 library(DBI)
 library(RMySQL)
 
+#Extraction des données avec l'API
 raw_data <- GET('https://api.jcdecaux.com/vls/v3/stations?contract=Lyon&apiKey=fc41d1b1016a6b95f0f755048a0690a80719ab50')
 
 df <- fromJSON ( rawToChar ( raw_data$content ) , flatten =  TRUE ) #Conversion des données en bloc de données
 
 #Geocodage
-
 #reverse <- df %>%
 #reverse_geocode(lat = position.latitude, long = position.longitude, method = 'osm',
 #address = address, full_results = TRUE)
@@ -45,6 +47,7 @@ geocode_csv <- read.csv2(file = "geocodage.csv", sep = ";",header = TRUE)
 geocode <- geocode_csv$df..reverse.postcode.
 df <- cbind(df, geocode)
 
+#Connexion à la base de données FreeSQLDatabase
 #bdd <- dbConnect(MySQL(),
 #                   user = "sql11645724" ,
 #                   password = "C7MWFfpDxs" ,
@@ -59,7 +62,7 @@ df_general <- df %>%
 df_position <- df %>%
   select(number, position.latitude, position.longitude,geocode)
 
-# Créez une table pour les informations sur les stands
+# Créez des tables pour les informations sur les stands
 df_totalStands <- df %>%
   select(number, totalStands.capacity, totalStands.availabilities.bikes, totalStands.availabilities.stands, totalStands.availabilities.mechanicalBikes, totalStands.availabilities.electricalBikes, totalStands.availabilities.electricalInternalBatteryBikes, totalStands.availabilities.electricalRemovableBatteryBikes)
 
