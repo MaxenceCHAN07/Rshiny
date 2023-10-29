23453b26a6baa7c33657d98181eed12b600b7d62
MaxenceCHAN07 <144699975+MaxenceCHAN07@users.noreply.github.com>
2023-10-29 10:18:27 +0100

    Update fichier_initial.R

--- fichier_initial.R
+++ fichier_initial.R
@@ -1,3 +1,4 @@
+#Installation des packages si besoin
 if (!require(httr)) {
   install.packages("httr")
 }
@@ -17,19 +18,19 @@ if (!require(DBI)) {
 if (!require(RMySQL)) {
   install.packages("RMySQL")
 }
-
+#Chargement des packages
 library(httr)
 library(jsonlite)
 library(dplyr)
 library(DBI)
 library(RMySQL)
 
+#Extraction des données avec l'API 
 raw_data <- GET('https://api.jcdecaux.com/vls/v3/stations?contract=Lyon&apiKey=fc41d1b1016a6b95f0f755048a0690a80719ab50')
 
 df <- fromJSON ( rawToChar ( raw_data$content ) , flatten =  TRUE ) #Conversion des données en bloc de données
 
 #Geocodage
-
 #reverse <- df %>%
 #reverse_geocode(lat = position.latitude, long = position.longitude, method = 'osm',
 #address = address, full_results = TRUE)
@@ -45,11 +46,13 @@ geocode_csv <- read.csv2(file = "geocodage.csv", sep = ";",header = TRUE)
 geocode <- geocode_csv$df..reverse.postcode.
 df <- cbind(df, geocode)
 
-#bdd <- dbConnect(MySQL(),
-#                   user = "sql11645724" ,
-#                   password = "C7MWFfpDxs" ,
-#                   host = "sql11.freesqldatabase.com" ,
-#                   dbname = "sql11645724",)
+#Connexion à la base de données FreeSQLDatabase
+bdd <- dbConnect(MySQL(),
+                  user = "sql11645724" ,
+                  password = "C7MWFfpDxs" ,
+                  host = "sql11.freesqldatabase.com" ,
+                  dbname = "sql11645724",)
+
 
 # Créez une table pour les données générales
 df_general <- df %>%
@@ -59,7 +62,7 @@ df_general <- df %>%
 df_position <- df %>%
   select(number, position.latitude, position.longitude,geocode)
 
-# Créez une table pour les informations sur les stands
+# Créez des tables pour les informations sur les stands
 df_totalStands <- df %>%
   select(number, totalStands.capacity, totalStands.availabilities.bikes, totalStands.availabilities.stands, totalStands.availabilities.mechanicalBikes, totalStands.availabilities.electricalBikes, totalStands.availabilities.electricalInternalBatteryBikes, totalStands.availabilities.electricalRemovableBatteryBikes)
 
